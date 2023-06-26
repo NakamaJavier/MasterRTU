5ffb4809c49cf57776d96aa9f64aae280f1ebdeb
JavierNakama <nakamajavier@hotmail.com>
2023-06-25 23:33:17 -0300

    Se comienza con logica de autoreconocimiento de slaves, con boton de refresh y reconocimiento forzado

--- MasterRTU/Form1.cs
+++ MasterRTU/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using EasyModbus;
+using System.IO;
 
 namespace MasterRTU
 {
@@ -15,6 +16,13 @@ namespace MasterRTU
     {
         ModbusClient ModClient = new ModbusClient();
         List<int> slaveIds = new List<int>();
+        private const string SlaveIdsFileName = "slaveIds.txt";
+        class SlaveObject
+        {
+            public int SlaveId { get; set; }
+            public int FailCount { get; set; }
+        }
+        List<SlaveObject> slaveList = new List<SlaveObject>();
 
         public Form1()
         {
@@ -37,26 +45,39 @@ namespace MasterRTU
                 timerPoll.Start();
                 lblStatus.Text = "Connected";
                 btnConnect.Text = "Disconect";
-                ModClient.ConnectionTimeout = 250;
-                slaveIds.Clear();
-                for (byte slaveId = 1; slaveId <= 15; slaveId++) // Itera sobre todos los posibles IDs de esclavos (1-255)
+                if (File.Exists(SlaveIdsFileName))
                 {
-                    try
-                    {
-                        ModClient.UnitIdentifier = slaveId;
-                        int[] response = ModClient.ReadHoldingRegisters(0, 1);
-                        // Si no se produce una excepción, significa que el esclavo con el ID actual está conectado
-                        slaveIds.Add(slaveId);
-                    }
-                    catch (Exception)
-                    {
-                        // Si se produce una excepción, el esclavo con el ID actual no está conectado
-                        // Puedes agregar un manejo de excepciones más específico si lo deseas
-                    }
+                    // Cargar los valores de slaveIds desde el archivo
+                    Console.WriteLine("Se cargaron los slaveIds desde un archivo");
+                    string[] lines = File.ReadAllLines(SlaveIdsFileName);
+                    slaveIds = lines.Select(int.Parse).ToList();
+                    CreateWriteRegistersComponents(353, 20, slaveIds);
+                    
                 }
-                Console.WriteLine("Cantidad de slaves conectados: " + slaveIds.Count);
-                CreateWriteRegistersComponents(353, 50, slaveIds);
-                ModClient.ConnectionTimeout = 1000;
+                else { 
+                    ModClient.ConnectionTimeout = 250;
+                    slaveIds.Clear();
+                    for (byte slaveId = 1; slaveId <= 15; slaveId++) // Itera sobre todos los posibles IDs de esclavos (1-255)
+                    {
+                        try
+                        {
+                            ModClient.UnitIdentifier = slaveId;
+                            int[] response = ModClient.ReadHoldingRegisters(0, 1);
+                            // Si no se produce una excepción, significa que el esclavo con el ID actual está conectado
+                            slaveIds.Add(slaveId);
+                        }
+                        catch (Exception)
+                        {
+                            // Si se produce una excepción, el esclavo con el ID actual no está conectado
+                            // Puedes agregar un manejo de excepciones más específico si lo deseas
+                        }
+                    }
+                    Console.WriteLine("Cantidad de slaves conectados: " + slaveIds.Count);
+                    CreateWriteRegistersComponents(353, 20, slaveIds);
+                    ModClient.ConnectionTimeout = 1000;
+                    File.WriteAllLines(SlaveIdsFileName, slaveIds.Select(id => id.ToString()));
+                    Console.WriteLine("Se creo un archivo de SlavesIds");
+                }
             }
             else
             {
@@ -80,26 +101,44 @@ namespace MasterRTU
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+        
             if (ModClient.Connected == true)
             {
                 foreach (byte slaveId in slaveIds)
                 {
-                    ModClient.UnitIdentifier = slaveId;
-                    bool[] vals = ModClient.ReadCoils(0, 3);
-                    int[] coilsAlert = new int[vals.Length];
-                    for (int i = 0; i < vals.Length; i++)
+                    SlaveObject slaveObject = slaveList.FirstOrDefault(slave => slave.SlaveId == slaveId);
+                    if (slaveObject == null)
                     {
-                        coilsAlert[i] = vals[i] ? 1 : 0;
+                        slaveObject = new SlaveObject { SlaveId = slaveId, FailCount = 0 };
+                        slaveList.Add(slaveObject);
                     }
-                    tCoil1_1.Text = coilsAlert[0].ToString();
-                    tCoil1_2.Text = coilsAlert[1].ToString();
-                    tCoil1_3.Text = coilsAlert[2].ToString();
-                    string textBoxName = "txtGrm" + slaveId;
-                    TextBox textBox = Controls.Find(textBoxName, true).FirstOrDefault() as TextBox;
-
-                    if (textBox != null && !string.IsNullOrEmpty(textBox.Text))
+                    if (slaveObject.FailCount < 6)
                     {
-                        ModClient.WriteSingleRegister(0, int.Parse(textBox.Text));
+                        try
+                        {
+                            ModClient.UnitIdentifier = slaveId;
+                            bool[] vals = ModClient.ReadCoils(0, 3);
+                            int[] coilsAlert = new int[vals.Length];
+                            for (int i = 0; i < vals.Length; i++)
+                            {
+                                coilsAlert[i] = vals[i] ? 1 : 0;
+                            }
+                            tCoil1_1.Text = coilsAlert[0].ToString();
+                            tCoil1_2.Text = coilsAlert[1].ToString();
+                            tCoil1_3.Text = coilsAlert[2].ToString();
+                            string textBoxName = "txtGrm" + slaveId;
+                            TextBox textBox = Controls.Find(textBoxName, true).FirstOrDefault() as TextBox;
+                            if (textBox != null && !string.IsNullOrEmpty(textBox.Text))
+                            {
+                                ModClient.WriteSingleRegister(0, int.Parse(textBox.Text));
+                            }
+                            slaveObject.FailCount = 0;
+                        }
+                        catch
+                        {
+                            Console.WriteLine("No se encontro el Slave:" + slaveId+" \n Error: "+ slaveObject.FailCount);
+                            slaveObject.FailCount++;
+                        }
                     }
                 }
                 
@@ -122,28 +161,10 @@ namespace MasterRTU
         }
 
         // Evento Form1_Load fuera de la clase Form1
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            // Lógica para agregar un botón dinámicamente
-            if (true)
-            {
-                Console.WriteLine("se genera el boton dinamico");
-                Button button = new Button();
-                button.Text = "Botón Dinámico";
-                button.Location = new System.Drawing.Point(520, 381);
-                button.Click += Button_Click; // Suscribir un controlador de eventos para el botón
-
-                this.Controls.Add(button);
-            }
-        }
-
-        private void Button_Click(object sender, EventArgs e)
-        {
-            // Lógica del controlador de eventos del botón dinámico
-            MessageBox.Show("¡Hiciste clic en el botón dinámico!");
-        }
+       
         private void CreateWriteRegistersComponents(int posicionX, int posicionY, List<int> slaveIds)
         {
+            
             //variables de formato:
             int indexlabel1Y = 70;
             int indexlabel2Y = indexlabel1Y+50;
@@ -173,28 +194,62 @@ namespace MasterRTU
                 labelSlave.Font = new Font(labelSlave.Font.FontFamily, 18, labelSlave.Font.Style);
                 labelSlave.AutoSize = true;
                 labelSlave.Location = new System.Drawing.Point(label1PosX, label1PosY);
-
-                Label labelGramos = new Label();
-                labelGramos.Text = "Cantidad de gramos:";
-                labelGramos.Font = new Font(labelGramos.Font.FontFamily, 12, labelGramos.Font.Style);
-                labelGramos.AutoSize = true;
-                labelGramos.Location = new System.Drawing.Point(label2PosX, label2PosY);
-
-                TextBox textBox = new TextBox();
-                textBox.Name = "txtGrm" + slaveIds[i];
-                textBox.Location = new System.Drawing.Point(textBoxPosX, textBoxPosY);
-                textBox.Font = new Font(textBox.Font.FontFamily, 12, textBox.Font.Style);
-
-                // Agregar los componentes al formulario
                 this.Controls.Add(labelSlave);
-                this.Controls.Add(labelGramos);
-                this.Controls.Add(textBox);
+
+                try
+                {
+                    ModClient.UnitIdentifier = Convert.ToByte(slaveIds[i]);
+                    int[] response = ModClient.ReadHoldingRegisters(0, 1);
+                    // Si no se produce una excepción, significa que el esclavo con el ID actual está conectado
+                    Label labelGramos = new Label();
+                    labelGramos.Text = "Cantidad de gramos:";
+                    labelGramos.Font = new Font(labelGramos.Font.FontFamily, 12, labelGramos.Font.Style);
+                    labelGramos.AutoSize = true;
+                    labelGramos.Location = new System.Drawing.Point(label2PosX, label2PosY);
+
+                    TextBox textBox = new TextBox();
+                    textBox.Name = "txtGrm" + slaveIds[i];
+                    textBox.Location = new System.Drawing.Point(textBoxPosX, textBoxPosY);
+                    textBox.Font = new Font(textBox.Font.FontFamily, 12, textBox.Font.Style);
+                    // Agregar los componentes al formulario
+                    
+                    this.Controls.Add(labelGramos);
+                    this.Controls.Add(textBox);
+                }
+                catch (Exception)
+                {
+                    Label labelGramos = new Label();
+                    labelGramos.Text = "DESCONECTADO";
+                    labelGramos.Font = new Font(labelGramos.Font.FontFamily, 12, labelGramos.Font.Style);
+                    labelGramos.AutoSize = true;
+                    labelGramos.Location = new System.Drawing.Point(label2PosX, label2PosY);
+                    this.Controls.Add(labelGramos);
+                    // Si se produce una excepción, el esclavo con el ID actual no está conectado
+                    // Puedes agregar un manejo de excepciones más específico si lo deseas
+                }
+
+                
 
                 // Incrementar las posiciones para el siguiente conjunto de componentes
                 label1PosY += 100;
                 label2PosY += 100;
                 textBoxPosY += 100;
             }
+            Button btnRefresh = new Button();
+            btnRefresh.Text = "Refresh";
+            btnRefresh.Name = "btnRefresh";
+            btnRefresh.Font = new Font(btnRefresh.Font.FontFamily, 18, btnRefresh.Font.Style);
+            btnRefresh.AutoSize = true;
+            btnRefresh.Location = new System.Drawing.Point(label1PosX, label1PosY);
+            this.Controls.Add(btnRefresh);
+            Button btnRecognize = new Button();
+            btnRecognize.Text = "Recognize Slaves";
+            btnRecognize.Name = "btnRecognize";
+            btnRecognize.Font = new Font(btnRecognize.Font.FontFamily, 18, btnRecognize.Font.Style);
+            btnRecognize.AutoSize = true;
+            btnRecognize.Location = new System.Drawing.Point(label1PosX+120, label1PosY);
+            this.Controls.Add(btnRecognize);
+
         }
     }
 
